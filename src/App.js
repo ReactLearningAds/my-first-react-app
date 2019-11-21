13f5e381ea9f8d48e6d7522cf77675194de8d4d6
Amit Kumar <amit.kumar@sapientrazorfish.com>
2019-11-20 21:38:36 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Hello from './Hello';
+import Welcome from './Welcome';
+import StatelessWelcome from './StatelessWelcome';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+     <Welcome message="This message is passed by the parent component to the child component"></Welcome>
+
+     <StatelessWelcome counter='0'/>
+
     </div>
   );
 }
